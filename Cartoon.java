5ab1608bdc64d9dacaf64ee1761e23a1183fba8f
Nick Sawicki <19sawickin@nicks-air-3.devices.brown.edu>
2021-03-01 15:14:03 -0500

    set locations

--- Cartoon.java
+++ Cartoon.java
@@ -1,9 +1,30 @@
 package cartoon;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.*;
+import javafx.util.Duration;
+
 public class Cartoon {
     // TODO: this is your top-level logic class! It should contain your event handlers, other "game" logic, and an instance of your composite shape!
     public Cartoon() {
 
+
+    }
+
+    public void setupTimeline() {
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), new TimeHandler());
+        Timeline timeline = new Timeline(kf);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private class TimeHandler implements EventHandler<ActionEvent> {
+
+        public void handle(ActionEvent event) {
+
+        }
     }
     
 }
--- House.java
+++ House.java
@@ -12,7 +12,26 @@ public class House {
     public House(BorderPane housePane) {
         _front = new Rectangle(Constants.HOUSE_WIDTH, Constants.HOUSE_HEIGHT);
         _front.setFill(Color.RED);
-        _leftWindow = new Rectangle()
+        _leftWindow = new Rectangle(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        _leftWindow.setFill(Color.GRAY);
+        _rightWindow = new Rectangle(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        _rightWindow.setFill(Color.GRAY);
+        _door = new Rectangle(Constants.DOOR_WIDTH, Constants.DOOR_HEIGHT);
+        _door.setFill(Color.BLACK);
+        housePane.getChildren().addAll(_front, _leftWindow, _rightWindow, _door);
+        this.setLocation(Constants.DOOR_X_LOC, Constants.DOOR_Y_LOC);
+    }
+
+    public void setLocation(double x, double y) {
+        _front.setX(x);
+        _front.setY(y);
+        _leftWindow.setX(x + 0.1*Constants.HOUSE_WIDTH);
+        _leftWindow.setY(y + 0.125*Constants.HOUSE_HEIGHT);
+        _rightWindow.setX(x + Constants.HOUSE_WIDTH - Constants.WINDOW_WIDTH - 0.1*Constants.HOUSE_WIDTH);
+        _rightWindow.setY(y + 0.125*Constants.HOUSE_HEIGHT);
+        _door.setX(x + 0.5*Constants.HOUSE_WIDTH - 0.5*Constants.DOOR_WIDTH);
+        _door.setY(y + Constants.HOUSE_HEIGHT - Constants.DOOR_HEIGHT);
+
 
     }
 }
